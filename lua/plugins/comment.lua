7e5bd926056e5bfe8c7851ef6dc6f2629288b90f
1332927388 <1332927388@qq.com>
2024-01-08 00:57:45 +0800

    comment for cpp

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,6 +1,15 @@
 return {
-  "JoosepAlviste/nvim-ts-context-commentstring",
-  lazy = true,
-  cofig = function ()
-  end
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    lazy = true,
+    config = function()
+        require("ts_context_commentstring").setup({
+            enable_autocmd = false,
+            languages = {
+                c = "// %s",
+                cpp = "// %s",
+            },
+            config = {},
+            commentary_integration = {},
+        })
+    end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,70 +1,49 @@
 return {
-  { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
 
-  -- require("nvim-treesitter.install").prefer_git = true,
-  -- add more treesitter parsers
-  {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      "JoosepAlviste/nvim-ts-context-commentstring",
+    -- require("nvim-treesitter.install").prefer_git = true,
+    -- add more treesitter parsers
+    {
+        "nvim-treesitter/nvim-treesitter",
+        dependencies = {
+            "JoosepAlviste/nvim-ts-context-commentstring",
+        },
+        -- "nvim-treesitter/nvim-treesitter",
+        -- dependencies = {
+        --   "JoosepAlviste/nvim-ts-context-commentstring",
+        -- },
+        opts = {
+            highlight = { enable = true },
+            prefer_git = true,
+            ensure_installed = {
+                "python",
+                "bash",
+                "json",
+                "lua",
+                "vim",
+                "yaml",
+                "c",
+                "cpp",
+            },
+        },
     },
-    config = function()
-      require('ts_context_commentstring').setup({
-        enable_autocmd = false,
-        languages = {
-          c = "// %s",
-          cpp = "// %s",
-        },
-        config = {
 
-        },
-        commentary_integration = {
-
-        },
-      })
-    end
-    -- "nvim-treesitter/nvim-treesitter",
-    -- dependencies = {
-    --   "JoosepAlviste/nvim-ts-context-commentstring",
-    -- },
-    -- opts = {
-    --   context_commentstring = {
-    --     enable = true,
-    --     enable_autocmd = false,
-    --     config = {
-    --       c = "// %s",
-    --       cpp = "// %s",
-    --     },
-    --   },
-    --   prefer_git = true,
-    --   ensure_installed = {
-    --     "bash",
-    --     "json",
-    --     "lua",
-    --     "vim",
-    --     "yaml",
-    --     "c",
-    --     "cpp",
-    --   },
-    -- },
-  },
-
-  -- since `vim.tbl_deep_extend`, can only merge tables and not lists, the code above
-  -- would overwrite `ensure_installed` with the new value.
-  -- If you'd rather extend the default config, use the code below instead:
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      -- add tsx and treesittek
-      vim.list_extend(opts.ensure_installed, {
-        "c",
-        "cpp",
-        "cmake",
-        "ninja",
-        "python",
-        "rst",
-        "toml",
-      })
-    end,
-  },
+    -- since `vim.tbl_deep_extend`, can only merge tables and not lists, the code above
+    -- would overwrite `ensure_installed` with the new value.
+    -- If you'd rather extend the default config, use the code below instead:
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = function(_, opts)
+            -- add tsx and treesittek
+            vim.list_extend(opts.ensure_installed, {
+                "c",
+                "cpp",
+                "cmake",
+                "ninja",
+                "python",
+                "rst",
+                "toml",
+            })
+        end,
+    },
 }
